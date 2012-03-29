960bc1364646e3a02a21712dcc2029346339c48c
Thomas Rader <saosiris@gmail.com>
2012-03-28 18:58:49 -0600

    added tank class

---
+++ TJR_Final/TJR_Final/tank.h
@@ -0,0 +1,13 @@
+//
+//  tank.h
+//  TJR_Final
+//
+//  Created by default on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tank : UIView
+
+@end
